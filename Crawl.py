113537227db3471101cb33231262038472217229
Edward Peterson <cubemaster21@comcast.net>
2016-11-30 19:44:21 -0500

    Adding in basics for areas

--- Crawl.py
+++ Crawl.py
@@ -1,7 +1,22 @@
 #CRAWL MAIN FILE
 import Dictionary
+from Area import Area
+from Item import Item
+
+turnNumber = 1
+
+area = Area()
+cheese = Item("Cheese Wheel",10)
+
+
+area.objects.append(cheese)
+area.objects.append(cheese)
 
 
 while(True):
-	cmd = input(":>")
-	Dictionary.isValidCommand(cmd)
+	cmd = input("Turn: " + str(turnNumber) + "  >>")
+	if(Dictionary.isValidCommand(cmd)):
+		turnNumber = turnNumber + 1
+		Dictionary.processCommand(cmd, area)
+	else:
+		print("Not a recognized command")
--- Dictionary.py
+++ Dictionary.py
@@ -2,4 +2,14 @@ def isValidCommand(command):
 	command = command.lower()
 	parsed = command.split(" ")
 	if(parsed[0] == "eat"):
-		print("tada")
+		return True
+	if(parsed[0] == "examine"):
+		return True
+
+def processCommand(command, area):
+	command = command.lower()
+	parsed = command.split(" ")
+	if(parsed[0] == "examine"):
+		print("This room contains: ");
+		for x in area.objects:
+			print(x.name)
