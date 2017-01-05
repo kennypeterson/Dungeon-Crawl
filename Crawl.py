bb95ad2270222b3e1e38d815d778729d58cf2ad5
Edward Peterson <cubemaster21@comcast.net>
2017-01-04 20:26:45 -0500

    Added item templates
    items and areas introduction

--- Crawl.py
+++ Crawl.py
@@ -1,18 +1,49 @@
 #CRAWL MAIN FILE
 import Dictionary
+import json
+import os
 from Area import Area
 from Item import Item
+from pprint import pprint
+
+script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 
 turnNumber = 1
+item_dict = []
 
 area = Area()
 cheese = Item("Cheese Wheel",10)
 
-
 area.objects.append(cheese)
 area.objects.append(cheese)
 
+class ItemEntry:
+	filename = ""
+	json = ""
+	def __init__(self, filename, json):
+		self.filename = filename
+		self.json = json
 
+
+# loading items code
+def loadItem(filename):
+	rel_path = "items/" + filename
+	abs_file_path = os.path.join(script_dir, rel_path)
+	with open(abs_file_path, encoding='utf-8') as data_file:
+		data = json.loads(data_file.read())
+		return data
+
+for file in os.listdir(os.path.join(script_dir, "items/")):
+	jsonFile = loadItem(file)
+	item_dict.append(ItemEntry(file.replace(".json", ""), jsonFile))
+
+def findItem(name):
+	for item in item_dict:
+		if(item.filename == name):
+			return item
+
+
+#main loop
 while(True):
 	cmd = input("Turn: " + str(turnNumber) + "  >>")
 	if(Dictionary.isValidCommand(cmd)):
--- Item.py
+++ Item.py
@@ -12,26 +12,55 @@ class Item:
 		self.name = name;
 		self.weight= weight;
 
+	def getWeight(self):
+		return self.weight
+	def setValue(self, value):
+		self.value = value
+		return self
+
 class Container(Item):
 	contents = []
 
 	def __init__(self, name, weight):
 		Item.__init__(self, name, weight)
-		isWieldable = False;
+		isWieldable = False
+	def getWeight(self):
+		sum = weight;
+		for x in contents:
+			sum = sum + x.getWeight()
+		return sum
+	def addObject(self, object):
+		contents.append(object)
+	def removeObject(self, object):
+		contents.remove(object)
+
+class Weapon(Item):
+
+	def __init__(self):
+		damageValue = 1
 
 item_cheese = Item("Cheese Wheel", 2)
 item_stone1 = Item("Pebble", .5)
 item_stone2 = Item("Stone", 1)
 item_stone3 = Item("Rock", 15)
 item_stone4 = Item("Boulder", 50)
-item_hammer = Item("Hammer", 2)
-item_metal_rod = Item("Metal Rod", 2)
+item_hammer = Item("Hammer", 2).setValue(2)
+item_metal_rod = Item("Metal Rod", 2).setValue(1)
 item_string = Item("String", .1)
+item_bone = Item("Bone", 1).setValue(0.5)
+item_gold = Item("Gold Coin", 0.25)
+item_silver = Item("Silver", 0.25).setValue(0.5)
+
 item_key1 = Item("Key", 0)
 item_key2 = Item("Key", 0)
 item_key3 = Item("Key", 0)
 item_key4 = Item("Key", 0)
 item_key5 = Item("Key", 0)
 item_key6 = Item("Key", 0)
+
+item_chest = Container("Oak Chest", 30)
+item_sack = Container("Cloth Sack", 1)
+item_quiver = Container("Quiver", 1)
+
 
 
